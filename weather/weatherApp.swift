211e0a3fd0c8f0c7923ac8daf3d41a851c3e393d
lior mada <liorm2006@gmail.com>
2022-08-14 00:03:15 +0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by lior madav on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
